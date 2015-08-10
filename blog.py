eba8ce46f3e5f9652f04d4e6b8375a9a30cda0a7
Copser <iamrmot@gmail.com>
2015-08-10 06:56:35 +0200

    Configurating communication beetwen are blog application and posts database.

---
+++ blog.py
@@ -0,0 +1,18 @@
+blog.py - controller
+from flask import from flask import Flask, render_template, request, session, \
+    flash, redirect, url_for, g
+
+# database configuration
+DATABASE = 'blog.db'
+app = Flask(__name__)
+
+# pulls in app configuration by looking for  UPERCASE veriables
+app.config.from_object(__name__)
+
+
+# function used for connecting to the database
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE_PATH'])
+
+if __name__ == "__main__":
+    app.run(debug=True)
